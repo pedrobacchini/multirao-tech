543744f6cdefe9804cd28bc979ed9ca24da3f4b0
Pedro Bacchini <engenharia_bacchini@hotmail.com>
2019-06-08 15:33:39 -0300

    Configuração dos componentes iniciais da aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CandidatureComponent } from './candidature/candidature.component';
+import { RegisterProfileComponent } from './register-profile/register-profile.component';
+import { ServiceDetailsComponent } from './service-details/service-details.component';
+import { OngComponent } from './ong/ong.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CandidatureComponent,
+    RegisterProfileComponent,
+    ServiceDetailsComponent,
+    OngComponent
   ],
   imports: [
     BrowserModule
